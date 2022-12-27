953654a580b91898c8a8397db5cebd9077f6da80
M. Hamad <50104001+mahdyhamad@users.noreply.github.com>
2022-12-27 21:49:06 +0300

    Add depth-first search

---
+++ graph/dfs.py
@@ -0,0 +1,46 @@
+"""
+        1
+    /       \
+   2         3
+  / \       / \  
+ 4   5     6   7 
+
+
+Depth-first-search algorithm:
+
+Visit every level from top to buttom.
+This can be achieved using a queue.
+
+1- create an empty queue, add to it the root/the starting node.
+2- pop the top of the queue, add its children to the queue.
+3- perform the previous step until the queue is empty.
+
+"""
+
+from node import Node
+
+class DFS:
+
+    @staticmethod
+    def run(start_node: Node):
+        DFS._dfs(start_node)
+    
+    @staticmethod
+    def _dfs(node: Node):
+        if node:
+            print(node.val)
+            for i in node.children:
+                DFS._dfs(i)
+
+
+if __name__ == '__main__':
+    
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
+    node7 = Node(7)
+    node2 = Node(2, [node4, node5])
+    node3 = Node(3, [node6, node7])
+    node1 = Node(1, [node3, node2])
+    
+    DFS.run(node1)
